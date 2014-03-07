316191e50f4d2b1870e70d62f8a8c75da6c07cc8
Сергей <tovarov.piter@gmail.com>
2014-03-07 22:36:13 +0300

    Create fitches.js

---
+++ fitches.js
@@ -0,0 +1,27 @@
+// нам понадобится бибилиоека jQuery
+
+$(finction(){
+  // для начала забираем адресс страницы и отрезаем оттуда домен
+  var a = location.href.substr(19);
+  // перебираем верхние элементы навигации
+  $('.menu-sitemap-tree>ul>li').each(function(){
+    // првоеряем адресс ссылки - долэжен совпадать с нашим адресом 
+    if( $('a', this).attr('href') == a){ 
+      // если все верно - убираем класс - который отвечает за скрытие элементов
+      $(this).removeClass('close');
+      return false;
+    } 
+  }) 
+});
+
+
+// После оптимизации 
+
+
+$(finction(){
+  $('.menu-sitemap-tree>ul>li').each(function(){
+    if( $('a', this).attr('href') == location.href.substr(19) ){ 
+      $(this).removeClass('close'); return false
+    } 
+  }) 
+})
